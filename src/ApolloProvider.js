d019c72b632b4bb1309bd6eb4d3f9dedb4d3889f
Doğan Kartal <dogankartal75@gmail.com>
2021-09-06 00:01:39 +0300

    Edit proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://afternoon-peak-80198.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
